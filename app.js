795199f6306cfb129ad0c8638820f332ec45b4a3
Marleen Bonhof <marley@Marleens-MacBook-Pro.local>
2022-02-12 17:49:33 +0100

    added the express package and made a base setup for a server | props to Andrew Mead

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hallo wereld')
+// express is a http module which simplifies the server process
+// its a module from NPM
+const express = require('express');
+const app = express();
+// here we define a home route where each request that is the root; www.localhost:3000 responds to; Hoi
+// req and res stand for request and response - request is the incoming request from the browser client
+// and res is the response we are going to send back to the browser
+app.get('/', (req, res) => {
+  res.send('Hoi')
+})
+
+app.get('/about', (req, res) => {
+  res.send('the about page')
+})
+
+app.listen(1337)
