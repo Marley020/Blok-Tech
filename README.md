0e714cb29d2c3d12bd5507b3a27173c7e467a857
Marleen Bonhof <marley@re-boyds-145-109-137-250.wireless.hva.nl>
2022-02-28 12:37:34 +0100

    image

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Blok-Tech
+
+## Inhoud
+- _Introduction_
+- _Purpose & General Info_
+- _Installation_
+- _Contributing_
+- _Credits_
+- _License_
+
+
+### Introduction (About)
+
+Voor dit project hebben we de opdracht gekregen om een nieuwe manier van matching te introduceren, in de huidige wereld van standaard matching applicaties. Project Tech is daarnaast ook de basis voor een gezamelijk project.
+
+### Purpose & General Info (Features)
+
+Tijdens Project Tech ga ik een deel van een applicatie uitwerken voor een matching app. De focus van dit deel van de applicatie ligt op het koppelen van gebruikers aan de hand van hun interesses/smaak.
+
+### Installation
+
+### Contributing
+
+Feedback is altijd welkom
+
+### Credits
+
+Marleen Bonhof 500700911 Communication & Multimedia Design
+
+### License
+
+marleen.bonhof@hva.nl
--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 // express is a http module which simplifies the server process
 // its a module from NPM
 const express = require('express');
+const {engine} = require('express-handlebars');
 const app = express();
+// initialize handlebars
+app.engine('handlebars', engine())
+// set the view engine to be handlebars
+app.set('view engine', 'handlebars')
+// set the view directory equal to views
+app.set('views', './views')
 // here we define a home route where each request that is the root; www.localhost:3000 responds to; Hoi
 // req and res stand for request and response - request is the incoming request from the browser client
 // and res is the response we are going to send back to the browser
 app.get('/', (req, res) => {
-  res.send('Hoi')
+  res.render('home')
 })
 
 app.get('/about', (req, res) => {
-  res.send('the about page')
+  res.render('about')
+})
+
+app.get('/login', (req, res) => {
+  res.send('the login page')
+})
+
+app.get('*', (req, res) => {
+  res.send('Not Found...')
 })
 
 app.listen(1337)
